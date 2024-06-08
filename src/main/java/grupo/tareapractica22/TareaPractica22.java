68d804c0c3f25cc21e9965f97fb3ea1ba5af921f
Emerson1911 <1284662@clases.edu.sv>
2024-06-07 23:38:03 -0600

    Commit de Inicio Junto con Formulario Cuentas y su conexion junto funcionando al 75%

---
+++ src/main/java/grupo/tareapractica22/TareaPractica22.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package grupo.tareapractica22;
+
+/**
+ *
+ * @author emers
+ */
+public class TareaPractica22 {
+
+    public static void main(String[] args) {
+        FrmCuentas frmcuentas = new FrmCuentas();
+        frmcuentas.setVisible(true);
+    }
+}
